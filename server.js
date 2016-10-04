5708fcf37697460b2b03d40ec6397da8fee48b5b
anshulmak <anshul123makdani@gmail.com>
2016-10-04 19:26:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1', function (req,res) {
+    res.send("article 1 is requested and will be served here");
+    
+});
+app.get('/article2', function (req,res) {
+    res.send("article 2 is requested and will be served here");
+    
+});
+app.get('/article3', function (req,res) {
+    res.send("article 3 is requested and will be served here");
+    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
